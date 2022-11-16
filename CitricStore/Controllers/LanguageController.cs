2eff411cbc364ee0c7edb2900d999a4af9561b43
NguyenCanhPhong <Nguyencanhphong246@gmail.com>
2022-11-16 12:37:58 +0700

    ngôn ngữ

--- CitricStore/Controllers/LanguageController.cs
+++ CitricStore/Controllers/LanguageController.cs
@@ -23,5 +23,70 @@ namespace CitricStore.Controllers
             Response.Cookies.Add(cookie);
             return View();
         }
+
+        public ActionResult Login(string language)
+        {
+            if (!String.IsNullOrEmpty(language))
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(language);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+            }
+            HttpCookie cookie = new HttpCookie(name: "Languages");
+            cookie.Value = language;
+            Response.Cookies.Add(cookie);
+            return View();
+        }
+
+        public ActionResult Register(string language)
+        {
+            if (!String.IsNullOrEmpty(language))
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(language);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+            }
+            HttpCookie cookie = new HttpCookie(name: "Languages");
+            cookie.Value = language;
+            Response.Cookies.Add(cookie);
+            return View();
+        }
+
+        public ActionResult FogotPass(string language)
+        {
+            if (!String.IsNullOrEmpty(language))
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(language);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+            }
+            HttpCookie cookie = new HttpCookie(name: "Languages");
+            cookie.Value = language;
+            Response.Cookies.Add(cookie);
+            return View();
+        }
+
+        public ActionResult DetailsGame(string language)
+        {
+            if (!String.IsNullOrEmpty(language))
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(language);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+            }
+            HttpCookie cookie = new HttpCookie(name: "Languages");
+            cookie.Value = language;
+            Response.Cookies.Add(cookie);
+            return View();
+        }
+
+        public ActionResult DetailsApp(string language)
+        {
+            if (!String.IsNullOrEmpty(language))
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(language);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(language);
+            }
+            HttpCookie cookie = new HttpCookie(name: "Languages");
+            cookie.Value = language;
+            Response.Cookies.Add(cookie);
+            return View();
+        }
     }
 }
